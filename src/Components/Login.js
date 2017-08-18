e61998ce94e9dfd62bc8e08eae208bd86f3d98bf
maxwell <m.nelson.study@gmail.com>
2017-08-18 16:29:56 -0700

    Remove confirm pass from login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,19 +6,15 @@ export default class Login extends Component {
   state={
     email: '',
     pass: '',
-    cpass: '',
   }
 
   handleSubmit() {
     const { email, pass, cpass } = this.state;
-    if (pass === cpass) {
-      login(email, pass)
-        .catch((err) => {
-          this.setState({ err: 'Invalid Username / Password'});
-        })
-    }
-    this.setState({email: '', pass: '', cpass: ''});
-
+    login(email, pass)
+      .catch((err) => {
+        this.setState({ err: 'Invalid Username / Password'});
+      });
+    this.setState({email: '', pass: ''});
   }
 
   render() {
@@ -32,10 +28,6 @@ export default class Login extends Component {
         <label className="form-login">
           Password:
           <input type="password" name="password" value={this.state.pass} onChange={(e) => this.setState({ pass: e.target.value})} />
-        </label>
-        <label className="form-login">
-          Confirm Password:
-          <input type="password" name="confirmPassword" value={this.state.cpass} onChange={(e) => this.setState({ cpass: e.target.value})} />
         </label>
         <button onClick={() => this.handleSubmit()} >Submit</button>
       </div>
